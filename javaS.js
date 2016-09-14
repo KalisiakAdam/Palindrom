a36c4b2ac1270739756232f03446ce05e53da62c
KalisiakAdam <akalisiak@op.pl>
2016-09-14 09:31:23 +0200

    Update javaS.js

--- javaS.js
+++ javaS.js
@@ -1,14 +1,19 @@
-// function describe if inputted word is palindrom word?
-
-function polin(word) {
-    var wordPolin = "";
-    for (var j = word.length - 1; j >= 0; j--) {
-        wordPolin += word[j];
-    }
-    if (wordPolin === word) {
-        document.write(word + " is palindrom, wow");
-    } else {
-        document.write(word + " unfortunately is not a palindrom");
-    }
+function polin(){
+   var word = document.getElementById("word").value;
+   var wordPolin = "";
+        for (var j = word.length-1; j>=0; j--) {
+            wordPolin+= word[j];
+        }
+		if (wordPolin===word){
+		
+			
+			document.getElementById("yes").innerHTML = "Słowo " + word + " jest palindromem.";
+		}
+		else{
+		
+			document.getElementById("no").innerHTML = "Słowo " + word + " nie jest palindromem.";
+		}
+	
 }
-//polin("kajak");
+
+polin(word);
